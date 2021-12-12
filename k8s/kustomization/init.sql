09f1f470fae773ac2c7338863c63bcca7fdeabf0
Mihai Farcas <farcasmihai91@gmail.com>
2021-12-12 19:17:21 +0200

    feat: added k8s manifests and configured devspace
    * added manifests for PGO
    * added manifest and docker file for database migration
    * initialized the database migration framework
    * added database initialization script that installs various extensions
    * initialized the Vue 3 app with Vite and TailwindCSS

---
+++ k8s/kustomization/init.sql
@@ -0,0 +1,9 @@
+BEGIN;
+-- TODO: change public schema owner for added security
+ALTER SCHEMA public OWNER TO 'starter-owner';
+-- Some extensions require superuser privileges, so we create them before migration time.
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp" WITH SCHEMA public;
+CREATE EXTENSION IF NOT EXISTS citext WITH SCHEMA public;
+CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+COMMIT;
