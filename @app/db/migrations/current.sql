87b4c6aa0e344dd19635bb1595ba65431a549d3b
Mihai Farcas <farcasmihai91@gmail.com>
2021-12-14 10:25:21 +0200

    feat(db): added the initial DB migration (wip)

--- @app/db/migrations/current.sql
+++ @app/db/migrations/current.sql
@@ -1,1 +1,25 @@
--- Enter migration here
+-- Allow starter-server-authentication to switch to starter-server
+GRANT :SERVER_ROLE TO :SERVER_AUTHENTICATOR_ROLE;
+
+-- First migration: drop everything and start from scratch
+DROP SCHEMA IF EXISTS :DB_SCHEMA_NAME CASCADE;
+
+-- we dont want untrusted roles to be able to install or modify things in the pblic schema
+REVOKE ALL ON SCHEMA public FROM public;
+
+ALTER DEFAULT privileges REVOKE ALL ON sequences FROM public;
+
+ALTER DEFAULT privileges REVOKE ALL ON functions FROM public;
+
+-- give access to the public schema to the DB owner role
+GRANT ALL ON SCHEMA public TO :DATABASE_OWNER;
+
+CREATE SCHEMA :DB_SCHEMA_NAME;
+
+GRANT usage ON SCHEMA public, :DB_SCHEMA_NAME TO :SERVER_ROLE;
+
+ALTER DEFAULT privileges IN SCHEMA public, :DB_SCHEMA_NAME GRANT usage,
+SELECT
+  ON sequences TO :SERVER_ROLE;
+
+ALTER DEFAULT privileges IN SCHEMA public, :DB_SCHEMA_NAME GRANT EXECUTE ON functions TO :SERVER_ROLE;
